9767bc1080b9d9135ca915f35523b38886aff7de
Bryan <bryanparisot@hotmail.com>
2022-06-11 10:36:31 +0200

    feat(pages): create page

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation/Navigation";
+
+const Home = () => {
+    return(
+        <div>
+            <Navigation/>
+            <div>Home</div>
+        </div>
+    );
+};
+export default Home
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation/Navigation";
+
+const NotFound = () => {
+    return(
+        <div>
+            <Navigation/>
+            <div>404</div>
+        </div>
+    );
+};
+export default NotFound
