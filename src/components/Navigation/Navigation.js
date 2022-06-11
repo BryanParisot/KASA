190cc1d6f3f4eb5d1a9f98cf4f4b9688a400f2df
Bryan <bryanparisot@hotmail.com>
2022-06-11 11:39:23 +0200

    feat(navigation): build and add navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+    return(
+        <div className="navigation">
+        <img src="./pictures/LOGO.png" alt="Logo"/>
+            <div className="nav-link">
+                    <NavLink className="link" exact to="/" activeClassName="nav-active">
+                        Accueil
+                    </NavLink>
+                    <NavLink exact to="/about" activeClassName="nav-active">
+                        A propos
+                    </NavLink>
+            </div>
+        </div>
+    );
+};
+export default Navigation
