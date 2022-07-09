e656b1772890fec611e63f190622baadcfb8d64b
Bryan <bryanparisot@hotmail.com>
2022-07-09 13:20:45 +0200

    feat(app): return 404 if id not found

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+} from "react-router-dom";
 import About from "./pages/About";
 import Announce from "./pages/Announce";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { useFetch } from "./utils/hooks";
+
+const GuardedRoute = ({ component: Component, data, ...rest }) => {
+  const currentId = rest.computedMatch.params.id;
+  const userExists = data.find((user) => user.id === currentId);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        userExists ? <Component {...props} /> : <Redirect to="/404" />
+      }
+    />
+  );
+};
 
 const App = () => {
+  const data = useFetch("../json/data.json");
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
-        <Route path="/announce/:id" exact component={Announce} />
+        <GuardedRoute
+          path="/announce/:id"
+          exact
+          component={Announce}
+          data={data}
+        />
         <Route component={NotFound} />
       </Switch>
     </Router>
--- src/pages/Announce.js
+++ src/pages/Announce.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import Slider from "../components/Caroussel/Slider";
 import Chips from "../components/Chips/Chips";
 import DropDownLitlle from "../components/DropDown/DropDownLittle";
+import Footer from "../components/Footer/Footer";
 import Navigation from "../components/Navigation/Navigation";
 import Profils from "../components/Profil/Profils";
 import { useFetch } from "../utils/hooks";
@@ -11,38 +12,50 @@ const Announce = () => {
   const datas = useFetch("../json/data.json");
 
   return (
-    <div className="announce">
-      <Navigation />
-      {datas.map((item) =>
-        item.id === id ? (
-          <div>
-            <Slider images={item.pictures} />
-            <div className="contain_more_information">
-              <div className="contain_title_subtitle">
-                <h2>{item.title}</h2>
-                <p>{item.location}</p>
-                <div className="contain_chips">
-                  {item.tags.map((i) => (
-                    <Chips text={i} />
-                  ))}
+    <>
+      <div className="announce">
+        <Navigation logo='../pictures/LOGO.png' />
+        {datas.map((item) =>
+          item.id === id ? (
+            <div>
+              <Slider images={item.pictures} />
+              <div className="contain_more_information">
+                <div className="contain_title_subtitle">
+                  <h2>{item.title}</h2>
+                  <p>{item.location}</p>
+                  <div className="contain_chips">
+                    {item.tags.map((i) => (
+                      <Chips text={i} />
+                    ))}
+                  </div>
+                </div>
+
+                <div className="contain_profil">
+                  <Profils
+                    rating={item.rating}
+                    name={item.host.name}
+                    img={item.host.picture}
+                  />
                 </div>
               </div>
-
-              <div className="contain_profil">
-                <Profils name={item.host.name} img={item.host.picture} />
+              <div className="contain_dropdown">
+                <DropDownLitlle
+                  title="description"
+                  content={item.description}
+                />
+                <DropDownLitlle
+                  title="equipments"
+                  equipement={item.equipments}
+                />
               </div>
             </div>
-
-            <div className="contain_dropdown">
-              <DropDownLitlle title="description" content={item.description} />
-              <DropDownLitlle title="equipments" equipement={item.equipments} />
-            </div>
-          </div>
-        ) : (
-          ""
-        )
-      )}
-    </div>
+          ) : (
+            ""
+          )
+        )}
+      </div>
+      <Footer img="../pictures/LOGO-WHITE.png" />
+    </>
   );
 };
 export default Announce;
