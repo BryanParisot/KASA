9b8953b3576ec8cd3f05a9f0c4ecbb577d5b1524
Bryan <bryanparisot@hotmail.com>
2022-07-24 22:44:13 +0200

    style(all) responsive

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,10 +6,12 @@ const NotFound = () => {
   return (
     <>
       <div className="not-found">
-        <Navigation  logo='./pictures/LOGO.png'/>
+        <Navigation logo="./pictures/LOGO.png" />
         <div className="contain-not-found">
-          <h2>404</h2>
-          <p>Oups! La page que vous demandez n'existe pas</p>
+          <div className="first_element">
+            <h2>404</h2>
+            <p>Oups! La page que vous demandez n'existe pas</p>
+          </div>
           <NavLink className="link-not-found" to="/">
             {" "}
             Retourner sur la page d'accueil
