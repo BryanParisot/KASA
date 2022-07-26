14061fc704143412480d189d54bb8db6367a138c
Bryan <bryanparisot@hotmail.com>
2022-07-26 09:35:42 +0200

    fix(carroussel) delete com

--- src/components/Caroussel/Slider.js
+++ src/components/Caroussel/Slider.js
@@ -1,14 +1,5 @@
 import React, { useState } from "react";
 
-// const data = [
-//   {
-//     img: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-//   },
-//   {
-//     img: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
-//   },
-// ];
-
 const Slider = (props) => {
   const [current, setCurrent] = useState(0);
   const length = props.images.length;
